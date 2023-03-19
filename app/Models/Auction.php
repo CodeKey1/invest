1cf5d78094d6bd2426e51eb9fcf8b9093c8bc818
Mohammad Yasser <moharidy98@gmail.comr>
2023-03-19 12:03:13 +0200

    add auction model complete

--- app/Models/Auction.php
+++ app/Models/Auction.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Auction extends Model
 {
     use HasFactory;
+
+    protected $table  = 'auction';
+
+    protected $fillable = ['id','name','date','label','note','created_at','updated_at'];
+
 }
